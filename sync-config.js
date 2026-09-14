3d07f4830bb223c4ece7361e8d5df896730d0433
jl7427678-ship-it <jl7427678@gmail.com>
2026-09-14 23:07:52 +0800

    Configure vocabulary sync endpoint

---
+++ sync-config.js
@@ -0,0 +1,4 @@
+window.ENGLISH_MEMORY_SYNC_CONFIG=Object.freeze({
+  supabaseUrl:'https://zbxzmokjeukloiftfajd.supabase.co',
+  supabasePublishableKey:'sb_publishable_oW5x0Ee8xsH15rqmeAQU8w_UByKb0yX'
+});
